447e1579cee37861b979f269f9c7403533af6f6d
dev-hemul <dev.hemulll@gmail.com>
2026-04-15 16:15:54 +0300

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const Body = styled.div`
   background-color: ${({theme}) => (theme.bg)};
@@ -42,9 +43,10 @@ function App() {
           <Projects />
           <Wrapper>
             <Education />
+            <Contact/>
           </Wrapper>
-          <Contact/>
         </Body>
+        <Footer />
       </Router>
     </ThemeProvider>
   )
